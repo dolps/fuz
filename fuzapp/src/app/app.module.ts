efb3026474b7fec8df1bffe8b869e9a5e3cfe23c
dolplads <dolplads@gmail.com>
2017-11-16 21:38:37 +0100

    authenticationService

--- fuzapp/src/app/app.module.ts
+++ fuzapp/src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { NgModule, ErrorHandler } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
+import {NgModule, ErrorHandler} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {IonicApp, IonicModule, IonicErrorHandler} from 'ionic-angular';
+import {MyApp} from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
+import {AboutPage} from '../pages/about/about';
+import {ContactPage} from '../pages/contact/contact';
+import {HomePage} from '../pages/home/home';
+import {TabsPage} from '../pages/tabs/tabs';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { AuthProvider } from '../providers/auth/auth';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {AuthProvider} from '../providers/auth/auth';
+import {LoginPage} from "../pages/login/login";
+import {SignupPage} from "../pages/signup/signup";
 
 @NgModule({
   declarations: [
@@ -18,6 +20,8 @@ import { AuthProvider } from '../providers/auth/auth';
     AboutPage,
     ContactPage,
     HomePage,
+    LoginPage,
+    SignupPage,
     TabsPage
   ],
   imports: [
@@ -30,13 +34,17 @@ import { AuthProvider } from '../providers/auth/auth';
     AboutPage,
     ContactPage,
     HomePage,
+    LoginPage,
+    SignupPage,
     TabsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    Storage, // used for local storage
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     AuthProvider
   ]
 })
-export class AppModule {}
+export class AppModule {
+}
--- fuzapp/src/providers/auth/auth.ts
+++ fuzapp/src/providers/auth/auth.ts
@@ -1,17 +1,67 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Storage} from "@ionic/storage";
+import {Headers} from "@angular/http";
 
 /*
-  Generated class for the AuthProvider provider.
+ Generated class for the AuthProvider provider.
 
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
+ See https://angular.io/guide/dependency-injection for more info on providers
+ and Angular DI.
+ */
 @Injectable()
 export class AuthProvider {
+  private token: any;
+  private CLOUD_API: string;
 
-  constructor(public http: HttpClient) {
-    console.log('Hello AuthProvider Provider');
+
+  constructor(public http: HttpClient, public storage: Storage) {
+    console.log('initializing authprovider');
+    this.CLOUD_API = 'https://YOUR_HEROKU_APP.herokuapp.com/api/auth/protected';
+  }
+
+  authenticate() {
+    return new Promise((resolve, reject) => {
+      this.storage.get('token').then((value) => {
+        this.token = value;
+
+        let headers = new HttpHeaders();
+        headers.append('Authorization', this.token);
+
+        this.http.get(this.CLOUD_API, {headers: headers})
+          .subscribe(res => {
+            resolve(res);
+          }, (err) => {
+            reject(err);
+          });
+      });
+    });
+  }
+
+  createAccount(details: any) {
+    return new Promise((resolve, reject) => {
+      let headers = new HttpHeaders();
+      headers.append('Content-Type', 'application/json');
+
+      this.http.post(this.CLOUD_API, JSON.stringify(details), {headers: headers})
+        .subscribe(res => {
+          let data = res[''];
+          this.token = data.token;
+          this.storage.set('token', this.token);
+
+          resolve(data);
+        }, err => {
+          reject(err);
+        })
+    });
+  }
+
+  login() {
+
+  }
+
+  logout() {
+    this.storage.set('token', '');
   }
 
 }
