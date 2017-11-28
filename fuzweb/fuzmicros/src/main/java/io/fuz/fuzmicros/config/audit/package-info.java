a04bab2871e4a89ace40928ea0f2b7bef03dbd13
dolplads <dolplads@gmail.com>
2017-11-28 20:45:37 +0100

    added kubernetes

---
+++ fuzweb/fuzmicros/src/main/java/io/fuz/fuzmicros/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package io.fuz.fuzmicros.config.audit;
---
+++ fuzweb/fuzmicros/src/main/java/io/fuz/fuzmicros/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package io.fuz.fuzmicros.web.rest.vm;
